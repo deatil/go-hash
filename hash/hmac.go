7aa5e0ea015460e6f75a860f7edd1d1839616e63
deatil <2217957370@qq.com>
2023-03-18 21:47:00 +0800

    更新

--- hash/hmac.go
+++ hash/hmac.go
@@ -1,134 +1,275 @@
 package hash
 
 import (
-    "crypto"
+    "hash"
+    "hash/adler32"
+    "crypto/md5"
+    "crypto/sha1"
     "crypto/hmac"
+    "crypto/sha256"
+    "crypto/sha512"
+
+    "golang.org/x/crypto/md4"
+    "golang.org/x/crypto/sha3"
+    "golang.org/x/crypto/ripemd160"
+    "github.com/deatil/go-hash/md2"
 )
 
 // NewHmac
-func (this Hash) NewHmac(hash crypto.Hash, secret []byte) Hash {
-    this.hash = hmac.New(hash.New, secret)
+// HMAC (Hash-based Message Authentication Code) 常用于接口签名验证
+// 支持的算法有:
+// md5、sha1、sha256、sha512、adler32、crc32、crc32b、crc32c、
+// fnv132、fnv164、fnv1a32、fnv1a64、
+// gost、gost-crypto、haval128,3、haval128,4、haval128,5、
+// haval160,3、haval160,4、haval160,5、haval192,3、haval192,4、
+// haval192,5、haval224,3、haval224,4、haval224,5、haval256,3、
+// haval256,4、haval256,5、joaat、md2、md4、
+// ripemd128、ripemd160、ripemd256、ripemd320、
+// sha224、sha3-224、sha3-256、sha3-384、sha3-512、
+// sha384、sha512/224、sha512/256、
+// snefru、snefru256、tiger128,3、tiger128,4、tiger160,3、
+// tiger160,4、tiger192,3、tiger192,4、whirlpool
+func (this Hash) NewHmac(h func() hash.Hash, secret []byte) Hash {
+    this.hash = hmac.New(h, secret)
 
     return this
+}
+
+// ============================================
+
+var (
+    newAdler32 = func() hash.Hash {
+        return adler32.New()
+    }
+)
+
+// HmacAdler32 哈希值
+func (this Hash) HmacAdler32(secret []byte) Hash {
+    this.data = hmacHash(newAdler32, this.data, secret)
+
+    return this
+}
+
+// NewHmacAdler32
+func (this Hash) NewHmacAdler32(secret []byte) Hash {
+    return this.NewHmac(newAdler32, secret)
+}
+
+// ============================================
+
+// HmacMd2 哈希值
+func (this Hash) HmacMd2(secret []byte) Hash {
+    this.data = hmacHash(md2.New, this.data, secret)
+
+    return this
+}
+
+// NewHmacMd2
+func (this Hash) NewHmacMd2(secret []byte) Hash {
+    return this.NewHmac(md2.New, secret)
 }
 
 // ============================================
 
 // HmacMd4 哈希值
 func (this Hash) HmacMd4(secret []byte) Hash {
-    this.data = hmacHash(crypto.MD4, this.data, secret)
+    this.data = hmacHash(md4.New, this.data, secret)
 
     return this
 }
 
 // NewHmacMd4
 func (this Hash) NewHmacMd4(secret []byte) Hash {
-    return this.NewHmac(crypto.MD4, secret)
+    return this.NewHmac(md4.New, secret)
 }
 
 // ============================================
 
 // HmacMd5 哈希值
 func (this Hash) HmacMd5(secret []byte) Hash {
-    this.data = hmacHash(crypto.MD5, this.data, secret)
+    this.data = hmacHash(md5.New, this.data, secret)
 
     return this
 }
 
 // NewHmacMd5
 func (this Hash) NewHmacMd5(secret []byte) Hash {
-    return this.NewHmac(crypto.MD5, secret)
+    return this.NewHmac(md5.New, secret)
 }
 
 // ============================================
 
 // HmacSHA1 哈希值
 func (this Hash) HmacSHA1(secret []byte) Hash {
-    this.data = hmacHash(crypto.SHA1, this.data, secret)
+    this.data = hmacHash(sha1.New, this.data, secret)
 
     return this
 }
 
 // NewHmacSHA1
 func (this Hash) NewHmacSHA1(secret []byte) Hash {
-    return this.NewHmac(crypto.SHA1, secret)
+    return this.NewHmac(sha1.New, secret)
 }
 
 // ============================================
 
 // HmacSha224 哈希值
 func (this Hash) HmacSha224(secret []byte) Hash {
-    this.data = hmacHash(crypto.SHA224, this.data, secret)
+    this.data = hmacHash(sha256.New224, this.data, secret)
 
     return this
 }
 
 // NewHmacSha224
 func (this Hash) NewHmacSha224(secret []byte) Hash {
-    return this.NewHmac(crypto.SHA224, secret)
+    return this.NewHmac(sha256.New224, secret)
 }
 
 // ============================================
 
 // HmacSha256 哈希值
 func (this Hash) HmacSha256(secret []byte) Hash {
-    this.data = hmacHash(crypto.SHA256, this.data, secret)
+    this.data = hmacHash(sha256.New, this.data, secret)
 
     return this
 }
 
 // NewHmacSha256
 func (this Hash) NewHmacSha256(secret []byte) Hash {
-    return this.NewHmac(crypto.SHA256, secret)
+    return this.NewHmac(sha256.New, secret)
 }
 
 // ============================================
 
 // HmacSha384 哈希值
 func (this Hash) HmacSha384(secret []byte) Hash {
-    this.data = hmacHash(crypto.SHA384, this.data, secret)
+    this.data = hmacHash(sha512.New384, this.data, secret)
 
     return this
 }
 
 // NewHmacSha384
 func (this Hash) NewHmacSha384(secret []byte) Hash {
-    return this.NewHmac(crypto.SHA384, secret)
+    return this.NewHmac(sha512.New384, secret)
 }
 
 // ============================================
 
 // HmacSha512 哈希值
 func (this Hash) HmacSha512(secret []byte) Hash {
-    this.data = hmacHash(crypto.SHA512, this.data, secret)
+    this.data = hmacHash(sha512.New, this.data, secret)
 
     return this
 }
 
 // NewHmacSha512
 func (this Hash) NewHmacSha512(secret []byte) Hash {
-    return this.NewHmac(crypto.SHA512, secret)
+    return this.NewHmac(sha512.New, secret)
+}
+
+// ============================================
+
+// HmacSha512_224 哈希值
+func (this Hash) HmacSha512_224(secret []byte) Hash {
+    this.data = hmacHash(sha512.New512_224, this.data, secret)
+
+    return this
+}
+
+// NewHmacSha512_224
+func (this Hash) NewHmacSha512_224(secret []byte) Hash {
+    return this.NewHmac(sha512.New512_224, secret)
+}
+
+// ============================================
+
+// HmacSha512_256 哈希值
+func (this Hash) HmacSha512_256(secret []byte) Hash {
+    this.data = hmacHash(sha512.New512_256, this.data, secret)
+
+    return this
+}
+
+// NewHmacSha512_256
+func (this Hash) NewHmacSha512_256(secret []byte) Hash {
+    return this.NewHmac(sha512.New512_256, secret)
 }
 
 // ============================================
 
 // HmacRipemd160 哈希值
 func (this Hash) HmacRipemd160(secret []byte) Hash {
-    this.data = hmacHash(crypto.RIPEMD160, this.data, secret)
+    this.data = hmacHash(ripemd160.New, this.data, secret)
 
     return this
 }
 
 // NewHmacRipemd160
 func (this Hash) NewHmacRipemd160(secret []byte) Hash {
-    return this.NewHmac(crypto.RIPEMD160, secret)
+    return this.NewHmac(ripemd160.New, secret)
+}
+
+// ============================================
+
+// HmacSHA3_224 哈希值
+func (this Hash) HmacSHA3_224(secret []byte) Hash {
+    this.data = hmacHash(sha3.New224, this.data, secret)
+
+    return this
+}
+
+// NewHmacSHA3_224
+func (this Hash) NewHmacSHA3_224(secret []byte) Hash {
+    return this.NewHmac(sha3.New224, secret)
+}
+
+// ============================================
+
+// HmacSHA3_256 哈希值
+func (this Hash) HmacSHA3_256(secret []byte) Hash {
+    this.data = hmacHash(sha3.New256, this.data, secret)
+
+    return this
+}
+
+// NewHmacSHA3_256
+func (this Hash) NewHmacSHA3_256(secret []byte) Hash {
+    return this.NewHmac(sha3.New256, secret)
+}
+
+// ============================================
+
+// HmacSHA3_384 哈希值
+func (this Hash) HmacSHA3_384(secret []byte) Hash {
+    this.data = hmacHash(sha3.New384, this.data, secret)
+
+    return this
+}
+
+// NewHmacSHA3_384
+func (this Hash) NewHmacSHA3_384(secret []byte) Hash {
+    return this.NewHmac(sha3.New384, secret)
+}
+
+// ============================================
+
+// HmacSHA3_512 哈希值
+func (this Hash) HmacSHA3_512(secret []byte) Hash {
+    this.data = hmacHash(sha3.New512, this.data, secret)
+
+    return this
+}
+
+// NewHmacSHA3_512
+func (this Hash) NewHmacSHA3_512(secret []byte) Hash {
+    return this.NewHmac(sha3.New512, secret)
 }
 
 // ============================================
 
 // 签名
-func hmacHash(hash crypto.Hash, message, secret []byte) []byte {
-    h := hmac.New(hash.New, secret)
+func hmacHash(hh func() hash.Hash, message, secret []byte) []byte {
+    h := hmac.New(hh, secret)
     h.Write(message)
 
     return h.Sum(nil)
---
+++ hash/hmac_test.go
@@ -0,0 +1,86 @@
+package hash
+
+import (
+    "fmt"
+    "testing"
+)
+
+var hmacMd5Tests = []struct {
+    input  string
+    secret string
+    output string
+}{
+    {"sdfgsdgfsdfg123132", "pass123", "bf4ce19abed97f9d852c5055d08fc188"},
+    {"dfg.;kp[jewijr0-34lsd", "pass123", "d273f8e00fad1b2bd6b048b2b6aa1cf2"},
+    {"123123", "pass123", "12c3626311950ac87858b935e1107269"},
+}
+
+func Test_HmacMD5(t *testing.T) {
+    assert := assertT(t)
+    assertError := assertErrorT(t)
+
+    for index, test := range hmacMd5Tests {
+        e := FromString(test.input).HmacMd5([]byte(test.secret))
+
+        t.Run(fmt.Sprintf("HmacMD5_test_%d", index), func(t *testing.T) {
+            assertError(e.Error, "HmacMD5")
+            assert(test.output, e.ToHexString(), "HmacMD5")
+        })
+    }
+}
+
+func Test_NewHmacMD5(t *testing.T) {
+    assert := assertT(t)
+    assertError := assertErrorT(t)
+
+    for index, test := range hmacMd5Tests {
+        e := Hashing().NewHmacMd5([]byte(test.secret)).
+            Write([]byte(test.input)).Sum(nil)
+
+        t.Run(fmt.Sprintf("NewHmacMD5_test_%d", index), func(t *testing.T) {
+            assertError(e.Error, "NewHmacMD5")
+            assert(test.output, e.ToHexString(), "NewHmacMD5")
+        })
+    }
+}
+
+// ===========
+
+var hmacSHA1Tests = []struct {
+    input  string
+    secret string
+    output string
+}{
+    {"sdfgsdgfsdfg123132", "pass123", "4ba7dc0364e2c375a0ba1fdacdf0a7a1d24b00d6"},
+    {"dfg.;kp[jewijr0-34lsd", "pass123", "11a43b22a8449fc36918873ac32fd5a99e466c3d"},
+    {"123123", "pass123", "2bc70da9a375a9d9481935eee5ac4e374b33ed8e"},
+}
+
+func Test_HmacSHA1(t *testing.T) {
+    assert := assertT(t)
+    assertError := assertErrorT(t)
+
+    for index, test := range hmacMd5Tests {
+        e := FromString(test.input).HmacSHA1([]byte(test.secret))
+
+        t.Run(fmt.Sprintf("HmacSHA1_test_%d", index), func(t *testing.T) {
+            assertError(e.Error, "HmacSHA1")
+            assert(test.output, e.ToHexString(), "HmacSHA1")
+        })
+    }
+}
+
+func Test_NewHmacSHA1(t *testing.T) {
+    assert := assertT(t)
+    assertError := assertErrorT(t)
+
+    for index, test := range hmacMd5Tests {
+        e := Hashing().NewHmacSHA1([]byte(test.secret)).
+            Write([]byte(test.input)).Sum(nil)
+
+        t.Run(fmt.Sprintf("NewHmacSHA1_test_%d", index), func(t *testing.T) {
+            assertError(e.Error, "NewHmacSHA1")
+            assert(test.output, e.ToHexString(), "NewHmacSHA1")
+        })
+    }
+}
