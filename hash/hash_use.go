826e702ab91ae8600d04b560d122667838ae158f
deatil <2217957370@qq.com>
2024-08-25 10:55:02 +0800

    优化

--- hash/hash_use.go
+++ hash/hash_use.go
@@ -9,43 +9,6 @@ import (
 // Type Mode
 var TypeMode = NewTypeSet[Mode, string](maxMode)
 
-// Type Name
-type TypeName interface {
-    ~uint | ~int
-}
-
-// Type Set
-type TypeSet[N TypeName, D any] struct {
-    // 最大值
-    max N
-
-    // 数据
-    names *DataSet[N, D]
-}
-
-// New TypeSet
-func NewTypeSet[N TypeName, D any](max N) *TypeSet[N, D] {
-    return &TypeSet[N, D]{
-        max:   max,
-        names: NewDataSet[N, D](),
-    }
-}
-
-// 生成新序列
-// Generate new id
-func (this *TypeSet[N, D]) Generate() N {
-    old := this.max
-    this.max++
-
-    return old
-}
-
-// 类型名称列表
-// name list
-func (this *TypeSet[N, D]) Names() *DataSet[N, D] {
-    return this.names
-}
-
 // Mode type
 type Mode uint
 
@@ -64,8 +27,6 @@ const (
     unknown Mode = 1 + iota
     maxMode
 )
-
-// ================
 
 // 接口
 type IHash interface {
